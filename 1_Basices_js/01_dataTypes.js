5efdfb6d224fffcfefb54518ec9619ef8d0bc0bd
Prakhar Mishra <94304386+prakhar17072004@users.noreply.github.com>
2025-12-12 19:19:42 +0000

    datatypes code is added

---
+++ 1_Basices_js/01_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"// treat all js code as newer version
+//alert(3+3) // we using node js not browser
+
+console.log(3+3); // code readability should be high 
+console.log("Prakhar mishra");
+
+// types of dataTypes
+
+ let name = "Prakhar " //string 
+ let age = 23;         // number
+
+ let isLoggedIn = false // boolean
+
+
+ //number => 2 to power 53
+ //bigint 
+ // string => ""
+ // boolean => true/false
+ //null => standalone value
+ // undefined => value is not assign
+ // symbol =>unique
+
+ // object 
+
+ console.log(typeof undefined)// undefine;
+ console.log(typeof null) // object
