9f38096a875101b04673ef7e3c01fd80a7a372b5
Prakhar Mishra <94304386+prakhar17072004@users.noreply.github.com>
2025-12-12 18:41:50 +0000

    varible and it types

---
+++ 1_Basices_js/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 1234;
+let accountEmail ="prakhar@1234";
+var accountPassword = "123455"
+accountCity = "Noida";
+
+//  accountId=2344; // not allowed
+ console.log(accountId)
+
+ accountEmail ="anljur@23345"
+ accountPassword ="345"
+ accountCity="Ayodhya"
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
