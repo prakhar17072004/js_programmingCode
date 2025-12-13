97a05f6cc0232d0581af01fa2b3a0db64d486945
Prakhar Mishra <94304386+prakhar17072004@users.noreply.github.com>
2025-12-13 18:10:35 +0000

    datatype summary is done

--- 1_Basices_js/dataType_summary.js
+++ 1_Basices_js/dataType_summary.js
@@ -4,3 +4,27 @@
 //Reference type (non-primitive)
 // Array, Objects, Fuctions
 // js is dynamic language
+
+const id = Symbol('1234')
+const anotherId = Symbol('1234')
+console.log(id);
+console.log(anotherId);
+
+
+console.log(id=== anotherId);
+
+// Arrays
+const freedomFighter = ["Bhagat Singh","Subhas chandra Bose","Chandrashekhar Azad"];
+// objects 
+  let myObject={
+    name:"Prakhar mishra",
+    age : 23,
+    dob : 17-7-2004,
+
+}
+
+ let myFunction=function (){
+    console.log("hello prakhar mishra how are you");
+    
+}
+myFunction()
