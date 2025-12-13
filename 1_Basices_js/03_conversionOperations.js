6c3635eca5efd87423adc905156a0c043d17a1a9
Prakhar Mishra <94304386+prakhar17072004@users.noreply.github.com>
2025-12-13 17:17:42 +0000

    operation examples is done

--- 1_Basices_js/03_conversionOperations.js
+++ 1_Basices_js/03_conversionOperations.js
@@ -29,3 +29,47 @@ console.log(booleanIsLoggedIn);
 // 1=>true , 0=>false
 // ""=> fasle
 //"Prakharmishra"// true
+
+//****************** Operations ****************************
+
+//console.log(2+2)
+//console.log(2-2)
+//console.log(2*2)
+//console.log(2**2)
+//console.log(2/2)
+//console.log(2%2)
+
+let str1= "hello"
+let str2 = " Prakhar"
+let str3 = str1 + str2
+console.log(str3);
+
+
+// some special situations
+ console.log(1+"2") //12
+ console.log("1"+ 2);//12
+ console.log("1"+2 +2);//122
+ console.log(1 +2+"2"); //32
+ console.log(true);// true
+ console.log(+true);//1 
+ console.log(+"");//0
+ //console.log(+false);//0
+ //console.log(true+);// syntaxerror 
+ //console.log(false+);// syntaxerror
+ console.log(0+0);
+ console.log(0*0)
+ console.log(1*0);
+ console.log(0);
+ 
+ 
+ 
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
