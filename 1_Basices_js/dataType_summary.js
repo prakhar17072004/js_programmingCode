38defbbab31be2c99d283b13c2782892efb2b2ad
Prakhar Mishra <94304386+prakhar17072004@users.noreply.github.com>
2025-12-13 17:57:04 +0000

    datatype summary is done

---
+++ 1_Basices_js/dataType_summary.js
@@ -0,0 +1,6 @@
+// Primitive dataTypes
+// 7 types : String , Number, Boolean , null , undefined Symbol,BigInt
+
+//Reference type (non-primitive)
+// Array, Objects, Fuctions
+// js is dynamic language
