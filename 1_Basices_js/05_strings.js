69a5a6f69d8131e14aef16171b61884091665259
Prakhar Mishra <94304386+prakhar17072004@users.noreply.github.com>
2025-12-14 17:46:50 +0000

    string is done

---
+++ 1_Basices_js/05_strings.js
@@ -0,0 +1,4 @@
+//console.log('prakhar'+' msihra');
+
+// using string interpolation 
+// string interpolation means embedding the variable or expression directly in string without using string concatenation (+).
