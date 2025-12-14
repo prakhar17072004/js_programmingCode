59b18ee87539f2f84e98310fe5fc676806f08ce3
Prakhar Mishra <94304386+prakhar17072004@users.noreply.github.com>
2025-12-14 17:50:57 +0000

    string is done

--- 1_Basices_js/05_strings.js
+++ 1_Basices_js/05_strings.js
@@ -2,3 +2,8 @@
 
 // using string interpolation 
 // string interpolation means embedding the variable or expression directly in string without using string concatenation (+).
+
+let name = 'Prakhar mishra'
+
+const repoCount = 40;
+console.log(`hey ${name} is a self motivated and self dependent person `);
