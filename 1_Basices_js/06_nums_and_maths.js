beb30bd25319191d5d6eb5c64dd49dac51ffa3b1
Prakhar Mishra <94304386+prakhar17072004@users.noreply.github.com>
2025-12-15 10:40:22 +0000

    number and Math function is done

---
+++ 1_Basices_js/06_nums_and_maths.js
@@ -0,0 +1,31 @@
+// const score = 400 // normal way to declear
+// console.log(score);
+
+// const balance = new Number(100) // that declear that it 100% number
+// console.log(balance);
+
+// console.log(balance.toString())
+// console.log(balance.toString().length);
+
+
+//+++++++++++++++++++++++++++++++++++++++++
+
+console.log(Math);
+console.log(Math.abs(-3));
+console.log(Math.round(4.5));
+console.log(Math.ceil(4.5));
+console.log(Math.floor(5.4));
+
+// math.random 
+
+console.log(Math.random());
+console.log((Math.random()*10)+1);//give value more than one
+console.log(Math.floor(Math.random()*10)+1)
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random()*(max-min+1))+min)
+
+
+
