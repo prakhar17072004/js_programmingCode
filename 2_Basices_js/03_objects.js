59e3b753e5f267ad14d3c886f21abbcab92030c9
Prakhar Mishra <94304386+prakhar17072004@users.noreply.github.com>
2025-12-15 12:55:50 +0000

    object is done

---
+++ 2_Basices_js/03_objects.js
@@ -0,0 +1,10 @@
+//objects literals
+
+const jsUser ={
+    name :"Prakhar",
+    age:23,
+    location:"Noida",
+    email:"prakahr@gmail.com",
+    isLoggedin :false,
+    lastLoginDays:["Monday","Saturday"] 
+}
