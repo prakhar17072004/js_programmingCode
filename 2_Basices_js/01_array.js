9c77e93a68ddda4cb4a783051dda8bbd218424b4
Prakhar Mishra <94304386+prakhar17072004@users.noreply.github.com>
2025-12-15 12:04:46 +0000

    array methods done

---
+++ 2_Basices_js/01_array.js
@@ -0,0 +1,15 @@
+//Array 
+ //how we create the array and how we use it method to communicate with array funtions 
+
+ const myArr = [0,1,3,4,5]
+ const myHeroes = ["Bhagat singh","Shukdev"]
+
+ const myArr2 = new Array(1,3,5,6)
+ console.log(myArr[4]);
+ // Array methods 
+
+ myArr.push(6)
+ console.log(myArr);
+ myArr.pop()
+ 
+ console.log(myArr);
