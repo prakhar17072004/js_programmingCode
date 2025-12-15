069e4a7513e6a0c54dc6b381981619f4108d72f8
Prakhar Mishra <94304386+prakhar17072004@users.noreply.github.com>
2025-12-15 11:09:12 +0000

    date and time funtions done

--- 1_Basices_js/07_dates.js
+++ 1_Basices_js/07_dates.js
@@ -1,14 +1,22 @@
 // Dates
 
 let myDate = new Date()
-console.log(myDate);// give in standard way
+// console.log(myDate);// give in standard way
 
-console.log(myDate.toString());//give date in string
-console.log(myDate.toLocaleString())// give it in local understanding
-console.log(myDate.getDate());// only give present day
-console.log(myDate.getDay());// give day of week
-console.log(myDate.getFullYear());// give year
-console.log(myDate.getHours());// give hours of standard clock
+// console.log(myDate.toString());//give date in string
+ console.log(myDate.toLocaleString())// give it in local understanding
+// console.log(myDate.getDate());// only give present day
+// console.log(myDate.getDay());// give day of week
+// console.log(myDate.getFullYear());// give year
+ console.log(myDate.getHours());// give hours of standard clock
+console.log(myDate.getMilliseconds());
+console.log(myDate.getMinutes());
+console.log(myDate.getMonth());
+console.log(myDate.getSeconds());
+console.log(myDate.getTime());
+console.log(myDate.getTimezoneOffset());
+console.log(myDate.getUTCDate());
+console.log(myDate.toDateString());
 
 
 
