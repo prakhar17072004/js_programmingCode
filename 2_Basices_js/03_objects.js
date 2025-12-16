4a46ba1d8e4cc084fbe22ea701917dcd47bfaa20
Prakhar Mishra <94304386+prakhar17072004@users.noreply.github.com>
2025-12-16 10:53:23 +0000

    objects done

--- 2_Basices_js/03_objects.js
+++ 2_Basices_js/03_objects.js
@@ -1,10 +1,41 @@
 //objects literals
-
+const mySym = Symbol("key1")
 const jsUser ={
     name :"Prakhar",
+    "full name ":"prakhar msihra",
+    [mySym] : "myKey2", //braket are used to represent symbols 
     age:23,
     location:"Noida",
     email:"prakahr@gmail.com",
     isLoggedin :false,
     lastLoginDays:["Monday","Saturday"] 
 }
+// console.log(jsUser.age);
+// console.log(jsUser.email);
+// console.log(jsUser.isLoggedin);
+// console.log(jsUser["full name "]);
+// console.log(jsUser[mySym]);
+// console.log(typeof jsUser[mySym]);
+
+// jsUser.email= "mprakhat@gmail.com"
+// Object.freeze(jsUser)
+// jsUser.email="ram@gmail.com"
+// console.log(jsUser);
+
+
+// function add karne 
+
+jsUser.greeting = function(){
+    console.log("hello prakhar");
+}
+
+jsUser.greeting2 = function(){
+    console.log(`hello js user ${this.name}`);
+    
+}
+console.log(jsUser.greeting());
+console.log(jsUser.greeting2());
+
+
+
+
---
+++ 2_Basices_js/04_objects.js
@@ -0,0 +1,6 @@
+//singleton object 
+//  const instaUser = new Object()// singleton object
+
+// const instaUser = {} // non -singleton object
+
+  console.log(instaUser)
