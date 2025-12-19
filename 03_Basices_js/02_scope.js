cea1288cf4f00b666fcd37dbf26221e0f72515ca
Prakhar Mishra <94304386+prakhar17072004@users.noreply.github.com>
2025-12-19 18:17:57 +0000

    scope is added

---
+++ 03_Basices_js/02_scope.js
@@ -0,0 +1,21 @@
+//let var and const
+
+
+
+{
+    let a= 30;
+const b= 40;
+var c = 46;
+
+    // console.log(a);
+    // console.log(b);
+    // console.log(c);
+    
+    
+    
+}
+// console.log(a);
+// console.log(b);
+console.log(c);
+
+
