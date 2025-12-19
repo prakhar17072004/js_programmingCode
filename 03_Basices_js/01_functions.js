e5296c048ae3d6d036cee492de87de0e3ca276f2
Prakhar Mishra <94304386+prakhar17072004@users.noreply.github.com>
2025-12-19 17:23:34 +0000

    new function is added

--- 03_Basices_js/01_functions.js
+++ 03_Basices_js/01_functions.js
@@ -2,7 +2,7 @@ const myNewArray = [200];
 
 
 function returnSecondValue(getArray){
-    return getArray[1]
+    return getArray[3]
 }
-console.log(returnSecondValue(myNewArray));
-loog
+// console.log(returnSecondValue(myNewArray));
+// console.log(returnSecondValue([200,400,600,700]));
