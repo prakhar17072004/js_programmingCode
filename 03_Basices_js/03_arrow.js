f090579ef40e566aab44e5f1c7ad19d7e3f7ff89
Prakhar Mishra <94304386+prakhar17072004@users.noreply.github.com>
2025-12-19 19:31:17 +0000

    username is added

--- 03_Basices_js/03_arrow.js
+++ 03_Basices_js/03_arrow.js
@@ -1,13 +1,46 @@
 // arrow funtion basices
 
-const user = {
-    username : "Prakhar ",
-    price : 999,
-    welcomeMessage:function(){
-        console.log(`${this.username},welcome to website`);
+// const user = {
+//     username : "Prakhar ",
+//     price : 999,
+//     welcomeMessage:function(){
+//         console.log(`${this.username},welcome to website`);
+//         console.log(this); // this refers the current context 
         
-    }
-}
-user.welcomeMessage();
-user.username="ram"
-user.welcomeMessage()
+//     }
+// }
+// user.welcomeMessage();
+// user.username="ram"
+// user.welcomeMessage()
+// console.log(this);//this refer the global context 
+
+
+// function chai(){
+//     console.log(this);
+    
+// }
+
+// chai()
+
+// const chai = function(){
+//     let username = "hitesh"
+//     console.log(this.username);
+    
+// }
+// const chai = () =>{ // this is arrow function
+//     let username = 'hitesh'
+//     console.log(this);
+    
+// }
+// chai()
+
+// arrow funtion
+//  const addTwo = (num1,num2)=>{
+//     return num1 +num2;
+//  }
+
+//  console.log(addTwo(3,4))
+
+// implicit return 
+
+  const 
