1c9c666837112fa459999f0465ead0e39e16b961
Prakhar Mishra <94304386+prakhar17072004@users.noreply.github.com>
2025-12-21 19:05:34 +0000

    while and do  while loop is added

---
+++ 05_iterations/03_loop.js
@@ -0,0 +1,15 @@
+// for of
+
+// const arr = [1,3,5,6,7,8];
+
+// for(const num of arr){
+//     console.log(num);
+    
+      
+// }
+
+const greetings = "Hello world"
+ for(const val of greetings){
+    console.log(val);
+    
+ }
